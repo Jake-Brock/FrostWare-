0b579d17a1cbba3cd0e20ec6af51fd9d8a9fb72c
Jake <frostwareofficial@gmail.com>
2025-06-30 04:14:00 +0100

    Update a1.lua

--- a1.lua
+++ a1.lua
@@ -2,12 +2,11 @@ getgenv()._frostw = "https://system.heatherx.site"
 getgenv()._dc = "https://getfrosts.xyz"
 getgenv()._frost = "https://raw.githubusercontent.com/Jake-Brock/FrostWare-/main/FrostWare%20Main.lua"
 
-local response = game:HttpGet(getgenv()._frostw .. "/check-access/" .. gethwid())
-local hasAccess = response:find('"valid"%s*:%s*true') ~= nil
+local response = tonumber(game:HttpGet(getgenv()._frostw .. "/status/" .. gethwid()))
 
-if hasAccess then
+if response and response > 0 then
     loadstring(game:HttpGet(getgenv()._frost))()
 else
     repeat task.wait(0.1) until game:IsLoaded()
-    loadstring(game:HttpGet(getgenv()._frostw .. "/init"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OneCreatorX/FrostWare/refs/heads/main/Exe.lua"))()
 end
